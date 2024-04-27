a89fe0858c4b3d8ad7534d8d55c1a105c75013d5
joejose <joel.jose@nokia.com>
2024-04-27 06:41:20 +0530

    Adding Postgres DB for Storage

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", store)
+	server := NewAPIServer(":3000", store)
 	server.Run()
 	fmt.Println("Yeah Buddy!")
 }
